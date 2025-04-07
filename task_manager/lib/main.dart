64046266515cff8c8007377013af5564b476dec2
BhavyaSontineni <bsontineni1@student.gsu.edu>
2025-04-07 12:14:57 -0400

    final app

--- task_manager/lib/main.dart
+++ task_manager/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+//BHAVYA SRI SAI -002893685
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
